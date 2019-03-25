d50ed1bf49da1b2766c46e243d0ee0306c406816
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-03-24 22:46:36 -0600

    Add cppcheck static analysis

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -5,3 +5,12 @@ set -e
 # Show information
 "${CXX}" --version
 qmake --version
+cppcheck --version
+
+# Static analysis
+cppcheck \
+	--enable=all \
+	--inconclusive \
+	--report-progress \
+	--std=c++11 \
+	src/
