5f3edb17d3435ab5a5817a3d220fc55d6fe0bc95
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2019-04-26 21:20:06 -0500

    Fix permissions for code coverage executable

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -5,6 +5,8 @@ set -e
 # code coverage
 travis_retry curl -L -o codacy-coverage-reporter "$(curl -L https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
 
+chmod +x ./codacy-coverage-reporter
+
 ./codacy-coverage-reporter report \
 	-l Java \
 	-r build/reports/jacoco/test/jacocoTestReport.xml
